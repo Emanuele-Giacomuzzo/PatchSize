bc0d745be081b985af5d70c429e236c9293b9417
Emanuele-Giacomuzzo <emanuelegiacomuzzo@gmail.com>
2022-06-20 13:39:51 +0200

    I cleaned the code to modify the dataset

--- r_files/PatchSizePilot_population.R
+++ r_files/PatchSizePilot_population.R
@@ -5,7 +5,7 @@ library("grid")
 library("gridExtra") 
 
 ### --- IMPORT DATASET --- ###
-info = read.csv("/Users/ema/github/PatchSizePilot/data/PatchSizePilot_dataset.csv", header = TRUE)
+culture_info = read.csv("/Users/ema/github/PatchSizePilot/data/PatchSizePilot_dataset.csv", header = TRUE)
 load("~/github/PatchSizePilot/data/population/t0.RData");t0 = pop_output
 load("~/github/PatchSizePilot/data/population/t1.RData"); t1 = pop_output
 load("~/github/PatchSizePilot/data/population/t2.RData"); t2 = pop_output
@@ -18,37 +18,11 @@ rm(pop_output)
 
 ### --- MODIFY DATASET --- ###
 
-#Modify datasets of time points
+#Fill columns in t0 - t7
 t0$time = "to find out"
 t1$time = "to find out"
-
-#Modify info dataset
-elongating_t0 = NULL
-for (video in 1:nrow(t0)){
-  for (ID in 1:nrow(info)) {
-    elongating_t0 = rbind(elongating_t0, t0[video,])
-  }
-}
-
-info$culture_ID = info$id; info$id = NULL
-culture_IDs = rep(1:nrow(info), times = nrow(t0))
-elongating_t0$culture_ID = culture_IDs
-
-#Change the column from replicate to replicate video so that it can be distinguished from treatment replicate
 t6$replicate_video = t6$replicate; t6$replicate = NULL
 t7$replicate_video = t7$replicate; t7$replicate = NULL
-
-#Merge each data point with its information
-t0 = merge(info,elongating_t0, by="culture_ID")
-t1 = merge(info,t1,by="culture_ID")
-t2 = merge(info,t2,by="culture_ID")
-t3 = merge(info,t3,by="culture_ID")
-t4 = merge(info,t4,by="culture_ID")
-t5 = merge(info,t5,by="culture_ID")
-t6 = merge(info,t6,by="culture_ID")
-t7 = merge(info,t7,by="culture_ID")
-
-#Give the replicate video 
 t0$replicate_video = 1:12
 t1$replicate_video = 1
 t2$replicate_video = 1
@@ -56,19 +30,37 @@ t3$replicate_video = 1
 t4$replicate_video = 1
 t5$replicate_video = 1
 
-#Bind all data points together 
-ds = rbind(t0, t1, t2, t3, t4, t5, t6, t7)
-rm(elongating_t0, t0, t1, t2, t3, t4, t5, t6, t7)
+culture_info$culture_ID = culture_info$id
+culture_info$id = NULL
 
-#Change column names
-ds$treatment_replicate = ds$replicate; ds$replicate = NULL
+elongating_t0 = NULL
+for (video in 1:nrow(t0)){
+  for (ID in 1:nrow(culture_info)) {
+    elongating_t0 = rbind(elongating_t0, t0[video,])
+  }
+}
+elongating_t0$culture_ID = rep(1:nrow(culture_info), times = nrow(t0))
 
-ds$system_nr = ds$metaecosystem
-ds$metaecosystem = NULL
+t0 = merge(culture_info,elongating_t0, by="culture_ID")
+t1 = merge(culture_info,t1,by="culture_ID")
+t2 = merge(culture_info,t2,by="culture_ID")
+t3 = merge(culture_info,t3,by="culture_ID")
+t4 = merge(culture_info,t4,by="culture_ID")
+t5 = merge(culture_info,t5,by="culture_ID")
+t6 = merge(culture_info,t6,by="culture_ID")
+t7 = merge(culture_info,t7,by="culture_ID")
+ds = rbind(t0, t1, t2, t3, t4, t5, t6, t7); rm(elongating_t0, t0, t1, t2, t3, t4, t5, t6, t7)
 
-#Transform the dates from time point to days from the start of the experiment
 ds$time_points = NULL
-ds$day = ds$time_point
+ds$indiv_ml = ds$indiv_per_volume*1000
+ds$treatment_replicate = ds$replicate; ds$replicate = NULL
+ds$system_nr = ds$metaecosystem; ds$metaecosystem = NULL
+ds$day = ds$time_point; ds$time_point = NULL
+ds$disturbance_ml = ds$disturbance.1; ds$disturbance.1 = NULL
+ds$metaecosystem_type = ds$metaeco; ds$metaeco = NULL
+ds$patch_size_ml = ds$patch_size; ds$patch_size = NULL
+ds$eco_metaeco_type = ds$ecosystem; ds$ecosystem = NULL
+
 ds$day[ds$day=="t0"] = "0"
 ds$day[ds$day=="t1"] = "4"
 ds$day[ds$day=="t2"] = "8"
@@ -79,47 +71,27 @@ ds$day[ds$day=="t6"] = "24"
 ds$day[ds$day=="t7"] = "28"
 ds$day = as.numeric(ds$day)
 
-#Set up column on whether a patch is part of a metaecosystem
-ds$metaecosystem[ds$metaeco=="L"] = "no"
-ds$metaecosystem[ds$metaeco=="L_L"] = "yes"
-ds$metaecosystem[ds$metaeco=="M"] = "no"
-ds$metaecosystem[ds$metaeco=="M_M"] = "yes"
-ds$metaecosystem[ds$metaeco=="S"] = "no"
-ds$metaecosystem[ds$metaeco=="S_L"] = "yes"
-ds$metaecosystem[ds$metaeco=="S_S"] = "yes"
-
-#Change disturbance volume column
-ds$disturbance_ml = ds$disturbance.1
-ds$disturbance.1 = NULL
-
-#Change metaeco column
-ds$metaeco[ds$metaeco=="S"] = NA
-ds$metaeco[ds$metaeco=="M"] = NA
-ds$metaeco[ds$metaeco=="L"] = NA
-
-#Create a new column of patch_size
-ds$patch_size_ml = ds$patch_size
 ds$patch_size[ds$patch_size_ml == 7.5] = "S"
 ds$patch_size[ds$patch_size_ml == 22.5] = "M"
 ds$patch_size[ds$patch_size_ml == 37.5] = "L"
 
-#create the ecosystem column 
-ds$ecosystem = paste0(ds$patch_size, " (", ds$metaeco, ")")
+ds$metaecosystem[ds$metaecosystem_type=="L"] = "no"
+ds$metaecosystem[ds$metaecosystem_type=="L_L"] = "yes"
+ds$metaecosystem[ds$metaecosystem_type=="M"] = "no"
+ds$metaecosystem[ds$metaecosystem_type=="M_M"] = "yes"
+ds$metaecosystem[ds$metaecosystem_type=="S"] = "no"
+ds$metaecosystem[ds$metaecosystem_type=="S_L"] = "yes"
+ds$metaecosystem[ds$metaecosystem_type=="S_S"] = "yes"
 
-#Reorder the levels of ecosystem 
-ds$ecosystem = factor(ds$ecosystem, 
-                      levels=c('S (S)', 'M (M)', 'L (L)', 'S (S_S)', 'S (S_L)', 'M (M_M)', 'L (L_L)', 'L (S_L)'))
+ds$metaecosystem_type[ds$metaecosystem_type=="S"] = NA
+ds$metaecosystem_type[ds$metaecosystem_type=="M"] = NA
+ds$metaecosystem_type[ds$metaecosystem_type=="L"] = NA
 
-#Transform individuals/microlitres to individuals/millilitres
-ds$indiv_ml = ds$indiv_per_volume*1000
+ds$eco_metaeco_type = paste0(ds$patch_size, " (", ds$metaecosystem_type, ")")
+ds$eco_metaeco_type = factor(ds$eco_metaeco_type, levels=c('S (S)', 'M (M)', 'L (L)', 'S (S_S)', 'S (S_L)', 'M (M_M)', 'L (L_L)', 'L (S_L)'))
 
-#Select columns, then change their names and orer 
-ds$metaeco_type = ds$metaeco; ds$metaeco = NULL
-ds$ecosystem_type = ds$ecosystem; ds$ecosystem = NULL
-ds$eco_metaeco_type = ds$ecosystem_type; ds$ecosystem_type = NULL
-
-ds = ds %>% select(culture_ID, patch_size, disturbance, metaeco_type, bioarea_per_volume, replicate_video, day, metaecosystem, system_nr, eco_metaeco_type, indiv_ml)
-col_order <- c("culture_ID", "disturbance", "day", "metaecosystem", "metaeco_type", "system_nr", "replicate_video", "patch_size", "eco_metaeco_type","bioarea_per_volume","indiv_ml")
+ds = ds %>% select(culture_ID, patch_size, disturbance, metaecosystem_type, bioarea_per_volume, replicate_video, day, metaecosystem, system_nr, eco_metaeco_type, indiv_ml)
+col_order <- c("culture_ID", "system_nr", "disturbance", "day", "patch_size", "metaecosystem", "metaecosystem_type", "eco_metaeco_type", "replicate_video","bioarea_per_volume","indiv_ml")
 ds = ds[, col_order]
 
 
@@ -128,71 +100,71 @@ ds = ds[, col_order]
 low.biomass.reg.raw = ds %>%
   filter ( disturbance == "low") %>%
   filter (metaecosystem == "yes") %>%
-  group_by(culture_ID, day, metaeco_type, system_nr, patch_size) %>%
+  group_by(culture_ID, day, metaecosystem_type, system_nr, patch_size) %>%
   summarise(bioarea_per_volume = mean(bioarea_per_volume)) %>%
-  group_by(day,metaeco_type, system_nr) %>%
+  group_by(day,metaecosystem_type, system_nr) %>%
   summarise(bioarea_per_volume = mean(bioarea_per_volume)) %>%
   ggplot (aes(x = reorder(day, sort(as.numeric(day))),
               y = bioarea_per_volume,
-              fill = metaeco_type,
-              color = metaeco_type)) +
+              fill = metaecosystem_type,
+              color = metaecosystem_type)) +
   geom_boxplot() +
   # labs(title = "Disturbance = low (raw data)") +
   xlab("Day") +
-  ylab("Regional biomass (bioarea/µl)") +
+  ylab("Regional biomass (average bioarea between 2 patches/µl)") +
   scale_y_continuous(limits = c(0, 5250))
 
 high.biomass.reg.raw = ds %>%
   filter ( disturbance == "high") %>%
   filter (metaecosystem == "yes") %>%
-  group_by(culture_ID, day, metaeco_type, system_nr, patch_size) %>%
+  group_by(culture_ID, day, metaecosystem_type, system_nr, patch_size) %>%
   summarise(bioarea_per_volume = mean(bioarea_per_volume)) %>%
-  group_by(day,metaeco_type, system_nr) %>%
+  group_by(day,metaecosystem_type, system_nr) %>%
   summarise(bioarea_per_volume = mean(bioarea_per_volume)) %>%
   ggplot (aes(x = reorder(day, sort(as.numeric(day))),
               y = bioarea_per_volume,
-              fill = metaeco_type,
-              color = metaeco_type)) +
+              fill = metaecosystem_type,
+              color = metaecosystem_type)) +
   geom_boxplot() +
   # labs(title = "Disturbance = high (raw data)") +
   xlab("Day") +
-  ylab("Regional biomass (bioarea/µl)") +
+  ylab("Regional biomass (average bioarea between 2 patches/µl)") +
   scale_y_continuous(limits = c(0,5250))
 
 low.biomass.reg.mean = ds %>%
   filter ( disturbance == "low") %>%
   filter (metaecosystem == "yes") %>%
-  group_by(culture_ID, day, metaeco_type, system_nr, patch_size) %>%
+  group_by(culture_ID, day, metaecosystem_type, system_nr, patch_size) %>%
   summarise(bioarea_per_volume = mean(bioarea_per_volume)) %>%
-  group_by(day,metaeco_type, system_nr) %>%
+  group_by(day,metaecosystem_type, system_nr) %>%
   summarise(bioarea_per_volume = mean(bioarea_per_volume)) %>%
   ggplot (aes(x = day,
               y = bioarea_per_volume,
-              fill = metaeco_type,
-              color = metaeco_type)) +
+              fill = metaecosystem_type,
+              color = metaecosystem_type)) +
   geom_point(stat = "summary", fun = "mean") +
   geom_line (stat = "summary", fun = "mean") +
   # labs(title = "Disturbance = low (averaged data)") +
   xlab("Day") +
-  ylab("Regional biomass (bioarea/µl)") +
+  ylab("Regional biomass (average bioarea between 2 patches/µl)") +
   scale_y_continuous(limits = c(0, 5250))
 
 high.biomass.reg.mean = ds %>%
   filter ( disturbance == "high") %>%
   filter (metaecosystem == "yes") %>%
-  group_by(culture_ID, day, metaeco_type, system_nr, patch_size) %>%
+  group_by(culture_ID, day, metaecosystem_type, system_nr, patch_size) %>%
   summarise(bioarea_per_volume = mean(bioarea_per_volume)) %>%
-  group_by(day,metaeco_type, system_nr) %>%
+  group_by(day,metaecosystem_type, system_nr) %>%
   summarise(bioarea_per_volume = mean(bioarea_per_volume)) %>%
   ggplot (aes(x = day,
               y = bioarea_per_volume,
-              fill = metaeco_type,
-              color = metaeco_type)) +
+              fill = metaecosystem_type,
+              color = metaecosystem_type)) +
   geom_point(stat = "summary", fun = "mean") +
   geom_line (stat = "summary", fun = "mean") +
   # labs(title = "Disturbance = high (averaged data)") +
   xlab("Day") +
-  ylab("Regional biomass (bioarea/µl)") +
+  ylab("Regional biomass (average bioarea between 2 patches/µl)") +
   scale_y_continuous(limits = c(0,5250))
 
 low.biomass.local.raw = ds %>%
@@ -306,71 +278,71 @@ high.biomass.closed.mean = ds %>%
 low.abundance.reg.raw = ds %>%
   filter ( disturbance == "low") %>%
   filter (metaecosystem == "yes") %>%
-  group_by(culture_ID, day, metaeco_type, system_nr, patch_size) %>%
+  group_by(culture_ID, day, metaecosystem_type, system_nr, patch_size) %>%
   summarise(indiv_ml = mean(indiv_ml)) %>%
-  group_by(day,metaeco_type, system_nr) %>%
+  group_by(day,metaecosystem_type, system_nr) %>%
   summarise(indiv_ml = mean(indiv_ml)) %>%
   ggplot (aes(x = reorder(day, sort(as.numeric(day))),
               y = indiv_ml,
-              fill = metaeco_type,
-              color = metaeco_type)) +
+              fill = metaecosystem_type,
+              color = metaecosystem_type)) +
   geom_boxplot() +
   # labs(title = "Disturbance = low (raw data)") +
   xlab("Day") +
-  ylab("Regional biomass (bioarea/µl)") +
+  ylab("Regional abundance (average individuals between 2 patches/µl)") +
   scale_y_continuous(limits = c(0,1750))
 
 high.abundance.reg.raw = ds %>%
   filter ( disturbance == "high") %>%
   filter (metaecosystem == "yes") %>%
-  group_by(culture_ID, day, metaeco_type, system_nr, patch_size) %>%
+  group_by(culture_ID, day, metaecosystem_type, system_nr, patch_size) %>%
   summarise(indiv_ml = mean(indiv_ml)) %>%
-  group_by(day,metaeco_type, system_nr) %>%
+  group_by(day,metaecosystem_type, system_nr) %>%
   summarise(indiv_ml = mean(indiv_ml)) %>%
   ggplot (aes(x = reorder(day, sort(as.numeric(day))),
               y = indiv_ml,
-              fill = metaeco_type,
-              color = metaeco_type)) +
+              fill = metaecosystem_type,
+              color = metaecosystem_type)) +
   geom_boxplot() +
   # labs(title = "Disturbance = high (raw data)") +
   xlab("Day") +
-  ylab("Regional biomass (bioarea/µl)") +
+  ylab("Regional abundance (average individuals between 2 patches/µl)") +
   scale_y_continuous(limits = c(0,1750))
   
 low.abundance.reg.mean = ds %>%
   filter ( disturbance == "low") %>%
   filter (metaecosystem == "yes") %>%
-  group_by(culture_ID, day, metaeco_type, system_nr, patch_size) %>%
+  group_by(culture_ID, day, metaecosystem_type, system_nr, patch_size) %>%
   summarise(indiv_ml = mean(indiv_ml)) %>%
-  group_by(day,metaeco_type, system_nr) %>%
+  group_by(day,metaecosystem_type, system_nr) %>%
   summarise(indiv_ml = mean(indiv_ml)) %>%
   ggplot (aes(x = day,
               y = indiv_ml,
-              fill = metaeco_type,
-              color = metaeco_type)) +
+              fill = metaecosystem_type,
+              color = metaecosystem_type)) +
   geom_point(stat = "summary", fun = "mean") +
   geom_line (stat = "summary", fun = "mean") +
   # labs(title = "Disturbance = low (averaged data)") +
   xlab("Day") +
-  ylab("Regional biomass (bioarea/µl)") +
+  ylab("Regional abundance (average individuals between 2 patches/µl)") +
   scale_y_continuous(limits = c(0,1750))
 
 high.abundance.reg.mean = ds %>%
   filter ( disturbance == "high") %>%
   filter (metaecosystem == "yes") %>%
-  group_by(culture_ID, day, metaeco_type, system_nr, patch_size) %>%
+  group_by(culture_ID, day, metaecosystem_type, system_nr, patch_size) %>%
   summarise(indiv_ml = mean(indiv_ml)) %>%
-  group_by(day,metaeco_type, system_nr) %>%
+  group_by(day,metaecosystem_type, system_nr) %>%
   summarise(indiv_ml = mean(indiv_ml)) %>%
   ggplot (aes(x = day,
               y = indiv_ml,
-              fill = metaeco_type,
-              color = metaeco_type)) +
+              fill = metaecosystem_type,
+              color = metaecosystem_type)) +
   geom_point(stat = "summary", fun = "mean") +
   geom_line (stat = "summary", fun = "mean") +
   # labs(title = "Disturbance = high (averaged data)") +
   xlab("Day") +
-  ylab("Regional biomass (bioarea/µl)") +
+  ylab("Regional abundance (average individuals between 2 patches/µl)") +
   scale_y_continuous(limits = c(0,1750))
 
 low.abundance.local.raw = ds %>%
@@ -496,3 +468,20 @@ grid.arrange(low.abundance.reg.raw, low.abundance.local.raw, low.abundance.close
 grid.arrange(high.abundance.reg.raw, high.abundance.local.raw, high.abundance.closed.raw, high.abundance.reg.mean, high.abundance.local.mean, high.abundance.closed.mean,
              ncol=3, nrow=2,
              top = textGrob("Abundance, disturbance = high",gp=gpar(fontsize=20,font=3)))
+
+# biomass_difference = NULL
+# ds_temp_2 = NULL
+# for (DAY in 1:max(ds$day)) {
+#   for (SYSTEM_NR in 1:max(ds$system_nr)){
+#     ds_temp = ds%>%
+#       filter(metaecosystem == "yes") %>%
+#       filter(day == DAY, system_nr == SYSTEM_NR)
+#     max = max(ds_temp$bioarea_per_volume)
+#     min = min(ds_temp$bioarea_per_volume)
+#     ds_temp_2$day = DAY
+#     ds_temp_2$system_nr = SYSTEM_NR
+#     ds_temp_2$metaecosystem_type = ds_temp[1,]$metaecosystem_type
+#     ds_temp_2$biomass_difference = (max-min)/min
+#     biomass_difference = rbind(biomass_difference, ds_temp_2)
+#   }
+# }
