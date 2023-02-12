96b3de744c13c33c6737abc9f09d85b5ae1b6707
Ema <emanuelegiacomuzzo@gmail.com>
2023-02-12 12:42:42 +0100

    .

---
+++ r_files/functions/plot.patches.median.body.size.points.R
@@ -0,0 +1,68 @@
+plot.patches.median.body.size.points = function(eco_metaeco_type_input) {
+  
+  response_variable = "median_body_size"
+  
+  ds_median_body_size %>%
+    filter(
+      disturbance == disturbance_input,
+      eco_metaeco_type %in% eco_metaeco_type_input
+    ) %>%
+    summarySE(measurevar = response_variable,
+              groupvars = c("day", "eco_metaeco_type")) %>%
+    ggplot(aes(
+      x = day,
+      y = get(response_variable),
+      group = interaction(day, eco_metaeco_type),
+      color = eco_metaeco_type
+    )) +
+    geom_point(stat = "summary",
+               fun = "mean",
+               position = position_dodge(dodging)) +
+    geom_line(
+      stat = "summary",
+      fun = "mean",
+      aes(group = eco_metaeco_type),
+      position = position_dodge(dodging)
+    ) +
+    geom_errorbar(aes(
+      ymax = get(response_variable) + ci,
+      ymin = get(response_variable) - ci
+    ),
+    width = width_errorbar,
+    position = position_dodge(dodging)) +
+    labs(x = axis_names$axis_name[axis_names$variable == "day"],
+         y = axis_names$axis_name[axis_names$variable == response_variable],
+         color = "") +
+    scale_color_manual(
+      values = c(
+        colours_treatments$colour[colours_treatments$treatment == eco_metaeco_type_input[1]][1],
+        colours_treatments$colour[colours_treatments$treatment == eco_metaeco_type_input[2]][1],
+        colours_treatments$colour[colours_treatments$treatment == eco_metaeco_type_input[3]][1],
+        colours_treatments$colour[colours_treatments$treatment == eco_metaeco_type_input[4]][1],
+        colours_treatments$colour[colours_treatments$treatment == eco_metaeco_type_input[5]][1],
+        colours_treatments$colour[colours_treatments$treatment == eco_metaeco_type_input[6]][1],
+        colours_treatments$colour[colours_treatments$treatment == eco_metaeco_type_input[7]][1],
+        colours_treatments$colour[colours_treatments$treatment == eco_metaeco_type_input[8]][1]
+      )
+    ) +
+    geom_vline(
+      xintercept = resource_flow_days,
+      linetype = resource_flow_line_type,
+      color = resource_flow_line_colour,
+      linewidth = resource_flow_line_width
+    ) +
+    # geom_hline(
+    #   yintercept = 0,
+    #   color = zero_line_colour,
+    #   linetype = zero_line_line_type,
+    #   linewidth = zero_line_line_width
+    # ) +
+    theme_bw() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      legend.position = legend_position
+    ) +
+    guides(color = guide_legend(nrow = 2,
+                                title.position = "top"))
+}
