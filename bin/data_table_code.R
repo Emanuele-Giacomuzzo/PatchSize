fae95520c2f729a023d236d88874c5d1dd33eb52
Ema <emanuelegiacomuzzo@gmail.com>
2023-02-20 17:20:29 +0100

    .

---
+++ bin/data_table_code.R
@@ -0,0 +1,5 @@
+datatable(ds_patches_effect_size,
+          rownames = FALSE,
+          options = list(scrollX = TRUE),
+          filter = list(position = 'top', 
+                        clear = FALSE))
