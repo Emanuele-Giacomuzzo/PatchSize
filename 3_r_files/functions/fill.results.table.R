a6118093924a0633abdfd02632aeacda0d780416
Ema <emanuelegiacomuzzo@gmail.com>
2024-10-01 17:48:57 +0200

    .

--- 3_r_files/functions/fill.results.table.R
+++ 3_r_files/functions/fill.results.table.R
@@ -5,16 +5,14 @@ fill.results.table = function(results_table,
                               model_stats_fixed){
   
   results_table = results_table %>%
-    add_row(
-      Response = response_variable,
-      Levels = paste(ecosystem_or_metaecosystem_type_input, collapse = " , "),
-      ΔAIC_full = model_stats_full$deltaAIC,
-      p_full = model_stats_full$p_value,
-      ΔR2_full = model_stats_full$R2,
-      ΔAIC_fix = model_stats_fixed$deltaAIC,
-      p_fix = model_stats_fixed$p_value,
-      ΔR2_fix = model_stats_fixed$R2
-    )
+    add_row(Response = response_variable,
+            Levels = paste(ecosystem_or_metaecosystem_type_input, collapse = " , "),
+            ΔAIC_full = model_stats_full$deltaAIC,
+            p_full = model_stats_full$p_value,
+            ΔR2_full = model_stats_full$R2,
+            ΔAIC_fix = model_stats_fixed$deltaAIC,
+            p_fix = model_stats_fixed$p_value,
+            ΔR2_fix = model_stats_fixed$R2)
   
   return(results_table)
   
