ca6edf3be40a646d916faafaba59650689d174b0
Ema <emanuelegiacomuzzo@gmail.com>
2024-11-01 10:46:29 +0100

    it works

--- 2_analysis/renv/library/macos/R-4.4/aarch64-apple-darwin20/glmmTMB/example_files/mk_examples.R
+++
@@ -1,5 +0,0 @@
-library(glmmTMB)
-salamander1 <- glmmTMB(count ~ mined + (1|site),
-                       zi=~mined,
-                       family=poisson, data=Salamanders)
-saveRDS(salamander1,"salamander1.rds",version=2)
--- 2_analysis/renv/library/macos/R-4.4/aarch64-apple-darwin20/glmmTMB/test_data/glmmTMB-test-funs.R
+++
@@ -1,5 +0,0 @@
-## remove leading/trailing white space, empty lines, multiple spaces
-squash_white <- function(x) {
-    x <- gsub(" +"," ",trimws(x))
-    return(x[nchar(x)>0])
-}
